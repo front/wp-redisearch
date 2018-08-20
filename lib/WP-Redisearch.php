808b96bb020052574bd05ca4b6d984676aff2c86
Foad Yousefi <foadyousefi@gmail.com>
2018-08-20 13:55:57 +0200

    Refactore WP-Redisearch.php file

--- lib/WP-Redisearch.php
+++ lib/WP-Redisearch.php
@@ -3,9 +3,6 @@
 namespace WPRedisearch;
 
 use WPRedisearch\Admin;
-use WPRedisearch\RediSearch\Index;
-use WPRedisearch\RediSearch\AddToIndex;
-use WPRedisearch\RedisRaw\PredisAdapter;
 
 /**
  * WPRedisearch Class.
@@ -16,15 +13,21 @@ use WPRedisearch\RedisRaw\PredisAdapter;
 
 class WPRedisearch {
 
+  private $admin;
   public function __construct() {
-    self::init();
+    $this->admin = new Admin;
+    $this->wp_redisearch_handle_ajax_requests();
   }
 
-  public static function init() {
-    new Admin;
-    $index = new Index;
-    $index->connect();
-    $index->create()->add();
-
+  /**
+  * Ajax actions listeners.
+  * @since    0.1.0
+  * @param
+  * @return object $fields
+  */
+  public function wp_redisearch_handle_ajax_requests() {
+    add_action('wp_ajax_wp_redisearch_add_to_index', array( $this->admin, 'wp_redisearch_add_to_index' ) );
+    add_action('wp_ajax_nopriv_wp_redisearch_add_to_index', array( $this->admin, 'wp_redisearch_add_to_index' ) );
   }
+
 }
