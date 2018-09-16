aac8fbbe98860987b2e787f9d14e60e0671ddcda
Foad Yousefi <foadyousefi@gmail.com>
2018-09-16 20:48:04 +0200

    Add npm script to compile es6 and minify it

---
+++ assets/scripts/admin.js
@@ -0,0 +1,86 @@
+(function ($) {
+  $(document).ready(function () {
+
+    var indexingOptions = $('.indexing-options')
+    var startIndexing = $('.start-indexing')
+    var resumeIndexing = $('.resume-indexing')
+
+    if (indexingOptions.attr('data-num-docs') == indexingOptions.attr('data-num-posts')) {
+      resumeIndexing.css('display', 'none')
+    }
+    
+    startIndexing.click(function (e) {
+      e.preventDefault()
+      startIndexing.css('display', 'none')
+      dropIndex()
+    })
+    
+    
+    resumeIndexing.click(function (e) {
+      e.preventDefault()
+      resumeIndexing.css('display', 'none')
+      addToIndex()
+    })
+      
+    function dropIndex() {
+      let postData = {
+        action: 'wp_redisearch_drop_index'
+      }
+      jQuery.ajax({
+        data: postData,
+        type: "post",
+        url: wpRds.ajaxUrl
+      })
+        .done(function (res) {
+          if (res.data) {
+            addToIndex()
+          }
+        })
+        .error(function (err) {
+          console.log(err)
+        })
+    }
+
+    function addToIndex() {
+      let postData = {
+        action: 'wp_redisearch_add_to_index'
+      }
+      jQuery.ajax({
+        data: postData,
+        type: "post",
+        url: wpRds.ajaxUrl
+      })
+        .done(function (res) {
+          if (res.data.offset < res.data.found_posts) {
+            updateProgressBar(res.data.offset, res.data.found_posts)
+            addToIndex()
+          } else {
+            updateProgressBar(res.data.found_posts, res.data.found_posts)
+          }
+        })
+        .error(function (err) {
+          console.log(err)
+        })
+    }
+
+    function updateProgressBar(numDocs, numPosts) {
+      let pBar = $("#indexBar")
+      let statNumDoc = $('#statNumDoc')
+      let statNumPosts = $('#statNumPosts')
+      statNumDoc.text(numDocs)
+      statNumPosts.text(numPosts)
+      let width = (numDocs / numPosts) * 100
+      pBar.css('width', width + '%')
+    }
+
+    renderProgressBar()
+    function renderProgressBar() {
+      let pBar = $("#indexBar")
+      let numDocs = pBar.attr('data-num-docs')
+      let numPosts = pBar.attr('data-num-posts')
+      let width = (numDocs / numPosts) * 100
+      pBar.css('width', width + '%')
+    }
+    
+  });
+} )( jQuery );
