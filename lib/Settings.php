a083ec524859fabb513908fb98c6a704fc44997b
Foad Yousefi <foadyousefi@gmail.com>
2018-09-24 23:25:37 +0200

    Remove use of carbon-fields

--- lib/Settings.php
+++ lib/Settings.php
@@ -13,11 +13,7 @@ class Settings {
   * @return string    $index_name
   */
   public static function indexName() {
-    if ( did_action('carbon_fields_register_fields') && did_action('carbon_fields_fields_registered') ) {
-      $index_name = carbon_get_theme_option( 'wp_redisearch_index_name' );
-    } else {
-      $index_name = get_option( '_wp_redisearch_index_name' );
-    }
+    $index_name = get_option( 'wp_redisearch_index_name' );
 
     if ( !isset( $index_name) ) {
       $site_url = get_bloginfo('wpurl');
@@ -37,11 +33,7 @@ class Settings {
   * @return string    $redis_server
   */
   public static function RedisServer() {
-    if ( did_action('carbon_fields_register_fields') && did_action('carbon_fields_fields_registered') ) {
-      $redis_server = carbon_get_theme_option( 'wp_redisearch_server' );
-    } else {
-      $redis_server = get_option( '_wp_redisearch_server' );
-    }
+    $redis_server = get_option( 'wp_redisearch_server' );
     return isset( $redis_server ) ? $redis_server : '127.0.0.1';
   }
 
@@ -52,11 +44,7 @@ class Settings {
   * @return string    $redis_port
   */
   public static function RedisPort() {
-    if ( did_action('carbon_fields_register_fields') && did_action('carbon_fields_fields_registered') ) {
-      $redis_port = carbon_get_theme_option( 'wp_redisearch_port' );
-    } else {
-      $redis_port = get_option( '_wp_redisearch_port' );
-    }
+    $redis_port = get_option( 'wp_redisearch_port' );
     return isset( $redis_port ) ? $redis_port : '6379';
   }
 
